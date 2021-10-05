c2b4787b3309bacc541013ae20ab208f4e139ee0
ParadoX <youneskazemi9798@gmail.com>
2021-10-05 15:25:45 +0330

    create custom user and manager user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from .managers import MyUserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=100, unique=True)
+    full_name = models.CharField(max_length=100)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    object = MyUserManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["full_name"]
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
