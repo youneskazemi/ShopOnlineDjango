8fd06d0c71d03eb3610dca14597110dc972fdd21
ParadoX <youneskazemi9798@gmail.com>
2021-10-05 16:00:59 +0330

    connect to postgresql

--- OnlineShopDjango/settings.py
+++ OnlineShopDjango/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'OnlineShopDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
