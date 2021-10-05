1656070361646850d347f7d51b543254110fc376
ParadoX <youneskazemi9798@gmail.com>
2021-10-05 21:05:59 +0330

    showing products

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+
 app_name = 'shop'
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('<slug:slug>/', views.product_detail, name="product_detail")
 ]
